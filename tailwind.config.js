e7fbef67cd0520abfe3af982ed229a91c8fc844a
Prakhargoel8c <rockingprakhar.987@outlook.com>
2022-10-08 08:55:21 +0530

    Added Name Text

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shine: 'shine 10s infinite linear',
+      },
+      keyframes: {
+        shine: {
+          from: { '-webkit-filter': 'hue-rotate(0deg)' },
+          to: { '-webkit-filter': 'hue-rotate(-360deg)' },
+        },
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
