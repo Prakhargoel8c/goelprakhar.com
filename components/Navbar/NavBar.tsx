60eb7fae5746ef8b1a875681ff482cd02559c4eb
Prakhargoel8c <rockingprakhar.987@outlook.com>
2022-10-15 20:24:04 +0530

    Setup husky and prettier

--- components/Navbar/NavBar.tsx
+++ components/Navbar/NavBar.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { createBreakpoint } from 'react-use';
-
-const useBreakpoint = createBreakpoint();
+import Image from 'next/image';
 
 export const NavBar = () => {
   return (
-    <nav className="bg-gray-900 border-gray-200 px-2 sm:px-4 py-2.5 rounded">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+    <nav className='bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded mt-2'>
+      <div className='container flex flex-wrap justify-between items-center mx-auto'>
+        <a href='https://flowbite.com/' className='flex items-center'>
+          <Image src='https://flowbite.com/docs/images/logo.svg' className='mr-3 h-6 sm:h-9' alt='Flowbite Logo' />
+          <span className='self-center text-xl font-semibold whitespace-nowrap text-white'>Prakhar</span>
         </a>
-        <button
+        {/* <button
           data-collapse-toggle="navbar-default"
           type="button"
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -71,7 +69,7 @@ export const NavBar = () => {
               </a>
             </li>
           </ul>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
